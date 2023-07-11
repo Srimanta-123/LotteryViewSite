bcc3058ec7e97c9f9413be1e213fb8968482fdd9
Srimanta-123 <129699706+Srimanta-123@users.noreply.github.com>
2023-07-11 13:27:59 +0530

    Update lottery.js

--- lottery.js
+++ lottery.js
@@ -3,23 +3,29 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
 const iconClose = document.querySelector('.icon-close');
+const btnPopupDropdown = document.querySelector('.btnLogin-popup_dropdown');
 
 
-
-registerLink.addEventListener('click',()=> {
+registerLink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
 
-loginLink.addEventListener('click',()=> {
+loginLink.addEventListener('click', () => {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click',()=> {
+btnPopup.addEventListener('click', () => {
     wrapper.classList.add('active-popup');
 });
 
-iconClose.addEventListener('click',()=> {
+iconClose.addEventListener('click', () => {
     wrapper.classList.remove('active-popup');
 });
 
- 
+btnPopupDropdown.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
+
+
+
+
